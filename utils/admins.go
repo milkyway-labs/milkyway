febb2340afda8075d60c9da0c0adde8fdfa34216
Hanjun Kim <hallazzang@gmail.com>
2024-05-24 00:45:52 +0900

    chore: change admin addresses for testing

--- utils/admins.go
+++ utils/admins.go
@@ -1,6 +1,5 @@
 package utils
 
 var Admins = map[string]bool{
-	"stride1k8c2m5cn322akk5wy8lpt87dd2f4yh9azg7jlh": true, // F5
-	"stride10d07y265gmmuvt4z0w9aw880jnsr700jefnezl": true, // gov module
+	"init163ynqsk9tnv5zted5v9upk9scgtnq2eqr6x7sh": true,
 }
