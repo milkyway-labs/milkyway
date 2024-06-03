7ac3769b05bb75efa3823205833afe405bf7e1f4
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-06-03 18:44:56 -0500

    feat: add keeper

---
+++ x/avs/keeper/keeper.go
@@ -0,0 +1,39 @@
+package keeper
+
+import (
+	"cosmossdk.io/log"
+	storetypes "cosmossdk.io/store/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/milkyway-labs/milkyway/x/avs/types"
+)
+
+type Keeper struct {
+	storeKey storetypes.StoreKey
+	cdc      codec.BinaryCodec
+	hooks    types.AVSHooks
+}
+
+// NewKeeper creates a new keeper
+func NewKeeper(cdc codec.BinaryCodec, storeKey storetypes.StoreKey) Keeper {
+	return Keeper{
+		storeKey: storeKey,
+		cdc:      cdc,
+	}
+}
+
+// Logger returns a module-specific logger.
+func (k *Keeper) Logger(ctx sdk.Context) log.Logger {
+	return ctx.Logger().With("module", "x/"+types.ModuleName)
+}
+
+// SetHooks allows to set the reactions hooks
+func (k *Keeper) SetHooks(rs types.AVSHooks) *Keeper {
+	if k.hooks != nil {
+		panic("cannot set avs hooks twice")
+	}
+
+	k.hooks = rs
+	return k
+}
