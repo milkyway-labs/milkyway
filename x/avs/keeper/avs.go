16837354fde1c7d6b644dc8fab58106a069a95cd
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-06-03 23:30:09 -0500

    feat: add keeper methods to save AVS information

---
+++ x/avs/keeper/avs.go
@@ -0,0 +1,67 @@
+package keeper
+
+import (
+	"cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/milkyway-labs/milkyway/x/avs/types"
+)
+
+// SetNextAVSID sets the next AVS ID to be used when registering a new AVS
+func (k *Keeper) SetNextAVSID(ctx sdk.Context, avsID uint32) {
+	store := ctx.KVStore(k.storeKey)
+	store.Set(types.NextAVSIDKey(), types.GetAVSIDBytes(avsID))
+}
+
+// HasNextAVSID checks if the next AVS ID is set
+func (k *Keeper) HasNextAVSID(ctx sdk.Context) bool {
+	store := ctx.KVStore(k.storeKey)
+	return store.Has(types.NextAVSIDKey())
+}
+
+// GetNextAVSID returns the next AVS ID to be used when registering a new AVS
+func (k *Keeper) GetNextAVSID(ctx sdk.Context) (avsID uint32, err error) {
+	store := ctx.KVStore(k.storeKey)
+	bz := store.Get(types.NextAVSIDKey())
+	if bz == nil {
+		return 0, errors.Wrapf(types.ErrInvalidGenesis, "initial avs id not set")
+	}
+
+	avsID = types.GetAVSIDFromBytes(bz)
+	return avsID, nil
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// SaveAVS stores a new AVS in the KVStore
+func (k *Keeper) SaveAVS(ctx sdk.Context, avs types.AVS) {
+	previous, existed := k.GetAVS(ctx, avs.ID)
+
+	// Save the AVS data
+	store := ctx.KVStore(k.storeKey)
+	store.Set(types.AVSStoreKey(avs.ID), k.cdc.MustMarshal(&avs))
+	k.Logger(ctx).Debug("saved avs", "id", avs.ID)
+
+	// Call the hook based on the AVS status change
+	switch {
+	case !existed:
+		k.AfterAVSCreated(ctx, avs.ID)
+	case previous.Status == types.AVS_STATUS_CREATED && avs.Status == types.AVS_STATUS_REGISTERED:
+		k.AfterAVSRegistered(ctx, avs.ID)
+	case previous.Status == types.AVS_STATUS_REGISTERED && avs.Status == types.AVS_STATUS_UNREGISTERED:
+		k.AfterAVSDeregistered(ctx, avs.ID)
+	}
+}
+
+// GetAVS returns an AVS from the KVStore
+func (k *Keeper) GetAVS(ctx sdk.Context, avsID uint32) (avs types.AVS, found bool) {
+	store := ctx.KVStore(k.storeKey)
+
+	bz := store.Get(types.AVSStoreKey(avsID))
+	if bz == nil {
+		return avs, false
+	}
+
+	k.cdc.MustUnmarshal(bz, &avs)
+	return avs, true
+}
--- x/avs/types/models.pb.go
+++ x/avs/types/models.pb.go
@@ -24,14 +24,58 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
+// AVSStatus defines the status of an AVS
+type AVSStatus int32
+
+const (
+	// AVS_STATUS_UNSPECIFIED defines the AVS status is unspecified
+	AVS_STATUS_UNSPECIFIED AVSStatus = 0
+	// AVS_STATUS_CREATED defines the AVS status is created
+	AVS_STATUS_CREATED AVSStatus = 1
+	// AVS_STATUS_REGISTERED defines the AVS status is updated
+	AVS_STATUS_REGISTERED AVSStatus = 2
+	// AVS_STATUS_UNREGISTERED defines the AVS status is unregistered
+	AVS_STATUS_UNREGISTERED AVSStatus = 3
+)
+
+var AVSStatus_name = map[int32]string{
+	0: "AVS_STATUS_UNSPECIFIED",
+	1: "AVS_STATUS_CREATED",
+	2: "AVS_STATUS_REGISTERED",
+	3: "AVS_STATUS_UNREGISTERED",
+}
+
+var AVSStatus_value = map[string]int32{
+	"AVS_STATUS_UNSPECIFIED":  0,
+	"AVS_STATUS_CREATED":      1,
+	"AVS_STATUS_REGISTERED":   2,
+	"AVS_STATUS_UNREGISTERED": 3,
+}
+
+func (x AVSStatus) String() string {
+	return proto.EnumName(AVSStatus_name, int32(x))
+}
+
+func (AVSStatus) EnumDescriptor() ([]byte, []int) {
+	return fileDescriptor_85e2c65ca3819b2c, []int{0}
+}
+
 // AVS defines the fields of an AVS
 type AVS struct {
 	// ID is the auto-generated unique identifier for the AVS
 	ID uint32 `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
-	// Name is the name of the AVS
-	Name string `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
+	// Status is the status of the AVS
+	Status AVSStatus `protobuf:"varint,2,opt,name=status,proto3,enum=milkyway.avs.v1.AVSStatus" json:"status,omitempty"`
 	// Admin is the address of the user that has registered the AVS
 	Admin string `protobuf:"bytes,3,opt,name=admin,proto3" json:"admin,omitempty"`
+	// Name is the name of the AVS
+	Name string `protobuf:"bytes,4,opt,name=name,proto3" json:"name,omitempty"`
+	// Description is the description of the AVS (optional)
+	Description string `protobuf:"bytes,5,opt,name=description,proto3" json:"description,omitempty"`
+	// Website is the website of the AVS (optional)
+	Website string `protobuf:"bytes,6,opt,name=website,proto3" json:"website,omitempty"`
+	// PictureURL is the URL of the picture of the AVS (optional)
+	PictureURL string `protobuf:"bytes,7,opt,name=pictureURL,proto3" json:"pictureURL,omitempty"`
 }
 
 func (m *AVS) Reset()         { *m = AVS{} }
@@ -74,11 +118,11 @@ func (m *AVS) GetID() uint32 {
 	return 0
 }
 
-func (m *AVS) GetName() string {
+func (m *AVS) GetStatus() AVSStatus {
 	if m != nil {
-		return m.Name
+		return m.Status
 	}
-	return ""
+	return AVS_STATUS_UNSPECIFIED
 }
 
 func (m *AVS) GetAdmin() string {
@@ -88,30 +132,69 @@ func (m *AVS) GetAdmin() string {
 	return ""
 }
 
+func (m *AVS) GetName() string {
+	if m != nil {
+		return m.Name
+	}
+	return ""
+}
+
+func (m *AVS) GetDescription() string {
+	if m != nil {
+		return m.Description
+	}
+	return ""
+}
+
+func (m *AVS) GetWebsite() string {
+	if m != nil {
+		return m.Website
+	}
+	return ""
+}
+
+func (m *AVS) GetPictureURL() string {
+	if m != nil {
+		return m.PictureURL
+	}
+	return ""
+}
+
 func init() {
+	proto.RegisterEnum("milkyway.avs.v1.AVSStatus", AVSStatus_name, AVSStatus_value)
 	proto.RegisterType((*AVS)(nil), "milkyway.avs.v1.AVS")
 }
 
 func init() { proto.RegisterFile("milkyway/avs/v1/models.proto", fileDescriptor_85e2c65ca3819b2c) }
 
 var fileDescriptor_85e2c65ca3819b2c = []byte{
-	// 241 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0xc9, 0xcd, 0xcc, 0xc9,
-	0xae, 0x2c, 0x4f, 0xac, 0xd4, 0x4f, 0x2c, 0x2b, 0xd6, 0x2f, 0x33, 0xd4, 0xcf, 0xcd, 0x4f, 0x49,
-	0xcd, 0x29, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x87, 0xc9, 0xea, 0x25, 0x96, 0x15,
-	0xeb, 0x95, 0x19, 0x4a, 0x89, 0xa4, 0xe7, 0xa7, 0xe7, 0x83, 0xe5, 0xf4, 0x41, 0x2c, 0x88, 0x32,
-	0x29, 0xc9, 0xe4, 0xfc, 0xe2, 0xdc, 0xfc, 0xe2, 0x78, 0x88, 0x04, 0x84, 0x03, 0x91, 0x52, 0x4a,
-	0xe4, 0x62, 0x76, 0x0c, 0x0b, 0x16, 0x12, 0xe3, 0x62, 0xca, 0x4c, 0x91, 0x60, 0x54, 0x60, 0xd4,
-	0xe0, 0x75, 0x62, 0x7b, 0x74, 0x4f, 0x9e, 0xc9, 0xd3, 0x25, 0x88, 0x29, 0x33, 0x45, 0x48, 0x88,
-	0x8b, 0x25, 0x2f, 0x31, 0x37, 0x55, 0x82, 0x49, 0x81, 0x51, 0x83, 0x33, 0x08, 0xcc, 0x16, 0xd2,
-	0xe3, 0x62, 0x4d, 0x4c, 0xc9, 0xcd, 0xcc, 0x93, 0x60, 0x06, 0x09, 0x3a, 0x49, 0x5c, 0xda, 0xa2,
-	0x2b, 0x02, 0x35, 0xd3, 0x31, 0x25, 0xa5, 0x28, 0xb5, 0xb8, 0x38, 0xb8, 0xa4, 0x28, 0x33, 0x2f,
-	0x3d, 0x08, 0xa2, 0xcc, 0xc9, 0xfd, 0xc4, 0x23, 0x39, 0xc6, 0x0b, 0x8f, 0xe4, 0x18, 0x1f, 0x3c,
-	0x92, 0x63, 0x9c, 0xf0, 0x58, 0x8e, 0xe1, 0xc2, 0x63, 0x39, 0x86, 0x1b, 0x8f, 0xe5, 0x18, 0xa2,
-	0x74, 0xd3, 0x33, 0x4b, 0x32, 0x4a, 0x93, 0xf4, 0x92, 0xf3, 0x73, 0xf5, 0x61, 0x3e, 0xd1, 0xcd,
-	0x49, 0x4c, 0x2a, 0x86, 0xf3, 0xf4, 0x2b, 0xc0, 0xfe, 0x2e, 0xa9, 0x2c, 0x48, 0x2d, 0x4e, 0x62,
-	0x03, 0x3b, 0xd9, 0x18, 0x10, 0x00, 0x00, 0xff, 0xff, 0x26, 0xf5, 0xc7, 0x42, 0x14, 0x01, 0x00,
-	0x00,
+	// 407 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x5c, 0x92, 0xcf, 0x6e, 0xd3, 0x30,
+	0x1c, 0xc7, 0xe3, 0xb4, 0xeb, 0x34, 0x23, 0xa0, 0xb2, 0x46, 0xf1, 0x02, 0x32, 0x15, 0xa7, 0x0a,
+	0xa9, 0x89, 0x36, 0x9e, 0x20, 0x5d, 0xc3, 0x54, 0x09, 0x21, 0x64, 0xb7, 0x3d, 0x70, 0xa9, 0xf2,
+	0xc7, 0x0a, 0x16, 0x4d, 0x1c, 0xc5, 0x6e, 0x46, 0x0f, 0xdc, 0x39, 0xf2, 0x0e, 0xbc, 0x02, 0x0f,
+	0xc1, 0x71, 0xe2, 0xc4, 0x09, 0xa1, 0xf4, 0x09, 0x78, 0x03, 0x34, 0x67, 0xad, 0x02, 0xb7, 0xdf,
+	0xf7, 0xfb, 0xf9, 0x28, 0xbf, 0xc8, 0x36, 0x7c, 0x9a, 0x89, 0xf5, 0x87, 0xed, 0x75, 0xb8, 0xf5,
+	0xc2, 0x4a, 0x79, 0xd5, 0xb9, 0x97, 0xc9, 0x84, 0xaf, 0x95, 0x5b, 0x94, 0x52, 0x4b, 0xf4, 0x70,
+	0x4f, 0xdd, 0xb0, 0x52, 0x6e, 0x75, 0xee, 0x9c, 0xa6, 0x32, 0x95, 0x86, 0x79, 0xb7, 0x53, 0xa3,
+	0x39, 0x67, 0xb1, 0x54, 0x99, 0x54, 0xab, 0x06, 0x34, 0xa1, 0x41, 0xcf, 0xff, 0x00, 0xd8, 0xf1,
+	0x97, 0x0c, 0x0d, 0xa0, 0x2d, 0x12, 0x0c, 0x86, 0x60, 0x74, 0x7f, 0xd2, 0xab, 0x7f, 0x3d, 0xb3,
+	0x67, 0x53, 0x6a, 0x8b, 0x04, 0x5d, 0xc0, 0x9e, 0xd2, 0xa1, 0xde, 0x28, 0x6c, 0x0f, 0xc1, 0xe8,
+	0xc1, 0x85, 0xe3, 0xfe, 0xb7, 0xd2, 0xf5, 0x97, 0x8c, 0x19, 0x83, 0xde, 0x99, 0xc8, 0x85, 0x47,
+	0x61, 0x92, 0x89, 0x1c, 0x77, 0x86, 0x60, 0x74, 0x32, 0xc1, 0x3f, 0xbe, 0x8d, 0x4f, 0xef, 0x96,
+	0xfa, 0x49, 0x52, 0x72, 0xa5, 0x98, 0x2e, 0x45, 0x9e, 0xd2, 0x46, 0x43, 0x08, 0x76, 0xf3, 0x30,
+	0xe3, 0xb8, 0x7b, 0xab, 0x53, 0x33, 0xa3, 0x21, 0xbc, 0x97, 0x70, 0x15, 0x97, 0xa2, 0xd0, 0x42,
+	0xe6, 0xf8, 0xc8, 0xa0, 0x76, 0x85, 0x30, 0x3c, 0xbe, 0xe6, 0x91, 0x12, 0x9a, 0xe3, 0x9e, 0xa1,
+	0xfb, 0x88, 0x08, 0x84, 0x85, 0x88, 0xf5, 0xa6, 0xe4, 0x0b, 0xfa, 0x1a, 0x1f, 0x1b, 0xd8, 0x6a,
+	0x5e, 0x7c, 0x82, 0x27, 0x87, 0x9f, 0x46, 0x0e, 0x1c, 0xf8, 0x4b, 0xb6, 0x62, 0x73, 0x7f, 0xbe,
+	0x60, 0xab, 0xc5, 0x1b, 0xf6, 0x36, 0xb8, 0x9c, 0xbd, 0x9a, 0x05, 0xd3, 0xbe, 0x85, 0x06, 0x10,
+	0xb5, 0xd8, 0x25, 0x0d, 0xfc, 0x79, 0x30, 0xed, 0x03, 0x74, 0x06, 0x1f, 0xb5, 0x7a, 0x1a, 0x5c,
+	0xcd, 0xd8, 0x3c, 0xa0, 0xc1, 0xb4, 0x6f, 0xa3, 0x27, 0xf0, 0xf1, 0x3f, 0x9f, 0x6b, 0xc1, 0x8e,
+	0xd3, 0xfd, 0xfc, 0x95, 0x58, 0x93, 0xab, 0xef, 0x35, 0x01, 0x37, 0x35, 0x01, 0xbf, 0x6b, 0x02,
+	0xbe, 0xec, 0x88, 0x75, 0xb3, 0x23, 0xd6, 0xcf, 0x1d, 0xb1, 0xde, 0x8d, 0x53, 0xa1, 0xdf, 0x6f,
+	0x22, 0x37, 0x96, 0x99, 0xb7, 0x3f, 0xe6, 0xf1, 0x3a, 0x8c, 0xd4, 0x21, 0x79, 0x1f, 0xcd, 0x3b,
+	0xd0, 0xdb, 0x82, 0xab, 0xa8, 0x67, 0xae, 0xf0, 0xe5, 0xdf, 0x00, 0x00, 0x00, 0xff, 0xff, 0x38,
+	0x4f, 0x20, 0x19, 0x24, 0x02, 0x00, 0x00,
 }
 
 func (m *AVS) Marshal() (dAtA []byte, err error) {
@@ -134,6 +217,34 @@ func (m *AVS) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
+	if len(m.PictureURL) > 0 {
+		i -= len(m.PictureURL)
+		copy(dAtA[i:], m.PictureURL)
+		i = encodeVarintModels(dAtA, i, uint64(len(m.PictureURL)))
+		i--
+		dAtA[i] = 0x3a
+	}
+	if len(m.Website) > 0 {
+		i -= len(m.Website)
+		copy(dAtA[i:], m.Website)
+		i = encodeVarintModels(dAtA, i, uint64(len(m.Website)))
+		i--
+		dAtA[i] = 0x32
+	}
+	if len(m.Description) > 0 {
+		i -= len(m.Description)
+		copy(dAtA[i:], m.Description)
+		i = encodeVarintModels(dAtA, i, uint64(len(m.Description)))
+		i--
+		dAtA[i] = 0x2a
+	}
+	if len(m.Name) > 0 {
+		i -= len(m.Name)
+		copy(dAtA[i:], m.Name)
+		i = encodeVarintModels(dAtA, i, uint64(len(m.Name)))
+		i--
+		dAtA[i] = 0x22
+	}
 	if len(m.Admin) > 0 {
 		i -= len(m.Admin)
 		copy(dAtA[i:], m.Admin)
@@ -141,12 +252,10 @@ func (m *AVS) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i--
 		dAtA[i] = 0x1a
 	}
-	if len(m.Name) > 0 {
-		i -= len(m.Name)
-		copy(dAtA[i:], m.Name)
-		i = encodeVarintModels(dAtA, i, uint64(len(m.Name)))
+	if m.Status != 0 {
+		i = encodeVarintModels(dAtA, i, uint64(m.Status))
 		i--
-		dAtA[i] = 0x12
+		dAtA[i] = 0x10
 	}
 	if m.ID != 0 {
 		i = encodeVarintModels(dAtA, i, uint64(m.ID))
@@ -176,11 +285,26 @@ func (m *AVS) Size() (n int) {
 	if m.ID != 0 {
 		n += 1 + sovModels(uint64(m.ID))
 	}
+	if m.Status != 0 {
+		n += 1 + sovModels(uint64(m.Status))
+	}
+	l = len(m.Admin)
+	if l > 0 {
+		n += 1 + l + sovModels(uint64(l))
+	}
 	l = len(m.Name)
 	if l > 0 {
 		n += 1 + l + sovModels(uint64(l))
 	}
-	l = len(m.Admin)
+	l = len(m.Description)
+	if l > 0 {
+		n += 1 + l + sovModels(uint64(l))
+	}
+	l = len(m.Website)
+	if l > 0 {
+		n += 1 + l + sovModels(uint64(l))
+	}
+	l = len(m.PictureURL)
 	if l > 0 {
 		n += 1 + l + sovModels(uint64(l))
 	}
@@ -242,10 +366,10 @@ func (m *AVS) Unmarshal(dAtA []byte) error {
 				}
 			}
 		case 2:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Name", wireType)
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Status", wireType)
 			}
-			var stringLen uint64
+			m.Status = 0
 			for shift := uint(0); ; shift += 7 {
 				if shift >= 64 {
 					return ErrIntOverflowModels
@@ -255,24 +379,11 @@ func (m *AVS) Unmarshal(dAtA []byte) error {
 				}
 				b := dAtA[iNdEx]
 				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
+				m.Status |= AVSStatus(b&0x7F) << shift
 				if b < 0x80 {
 					break
 				}
 			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthModels
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthModels
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Name = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
 		case 3:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Admin", wireType)
@@ -304,6 +415,134 @@ func (m *AVS) Unmarshal(dAtA []byte) error {
 				return io.ErrUnexpectedEOF
 			}
 			m.Admin = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 4:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Name", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowModels
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthModels
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthModels
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Name = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 5:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Description", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowModels
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthModels
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthModels
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Description = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 6:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Website", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowModels
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthModels
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthModels
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Website = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 7:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field PictureURL", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowModels
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthModels
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthModels
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.PictureURL = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
