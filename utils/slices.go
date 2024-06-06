fa873e7aaa534a2238bc9d42185d9071c2586da0
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-06-06 01:01:41 -0500

    refactor: make T any instead of comparable

--- utils/slices.go
+++ utils/slices.go
@@ -2,7 +2,7 @@ package utils
 
 // FindDuplicate returns the first duplicate element in the slice.
 // If no duplicates are found, it returns nil instead.
-func FindDuplicate[T comparable](slice []T, compare func(a T, b T) bool) *T {
+func FindDuplicate[T any](slice []T, compare func(a T, b T) bool) *T {
 	for i, a := range slice {
 		for j, b := range slice {
 			if i != j && compare(a, b) {
