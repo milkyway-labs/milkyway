531dccc34122ea019bd5fecef99f7cb865fe960b
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-06-10 08:12:57 -0500

    chore: add new keeper method

--- x/restaking/keeper/keeper.go
+++ x/restaking/keeper/keeper.go
@@ -1,9 +1,21 @@
 package keeper
 
+import (
+	storetypes "cosmossdk.io/store/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+)
+
 type Keeper struct {
+	storeKey storetypes.StoreKey
+	cdc      codec.Codec
+
 	authority string
 }
 
-func NewKeeper(authority string) *Keeper {
-
+func NewKeeper(cdc codec.Codec, storeKey storetypes.StoreKey, authority string) *Keeper {
+	return &Keeper{
+		storeKey:  storeKey,
+		cdc:       cdc,
+		authority: authority,
+	}
 }
