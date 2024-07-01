83fab63f8026f7aaeecdde9e12ce0723b7b915c6
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-07-01 13:49:45 -0500

    chore: add comments

--- x/restaking/types/delegation.go
+++ x/restaking/types/delegation.go
@@ -4,16 +4,20 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+// DelegationReceiver is an interface that represents the receiver of a delegation (operator, pool, service, etc).
 type DelegationReceiver interface {
 	GetID() uint32
 	GetAddress() string
 	InvalidExRate() bool
 }
 
+// DelegationGetter represents a function that allows to retrieve an existing delegation
 type DelegationGetter func(ctx sdk.Context, receiverID uint32, delegator string) (Delegation, bool)
 
+// DelegationBuilder represents a function that allows to build a new delegation
 type DelegationBuilder func(receiverID uint32, delegator string) Delegation
 
+// DelegationUpdater represents a function that allows to update an existing delegation
 type DelegationUpdater func(ctx sdk.Context, delegation Delegation) (newShares sdk.DecCoins, err error)
 
 // Delegation is an interface that represents a delegation object.
