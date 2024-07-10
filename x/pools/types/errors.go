9d8f175103b33b2833b90e6bf1826d830c484deb
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-07-10 11:34:26 -0500

    refactor: use sdkerrors.ErrNotFound instead of custom not found errors

--- x/pools/types/errors.go
+++ x/pools/types/errors.go
@@ -2,10 +2,11 @@ package types
 
 import (
 	"cosmossdk.io/errors"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 var (
+	ErrPoolNotFound       = errors.Wrap(sdkerrors.ErrNotFound, "pool not found")
 	ErrInvalidGenesis     = errors.Register(ModuleName, 1, "invalid genesis state")
 	ErrInsufficientShares = errors.Register(ModuleName, 2, "insufficient delegation shares")
-	ErrPoolNotFound       = errors.Register(ModuleName, 3, "pool not found")
 )
