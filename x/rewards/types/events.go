350dc230a4fb3c91e539f9a67e2961a371f71822
Hanjun Kim <hallazzang@gmail.com>
2024-08-19 17:59:21 +0900

    chore: add comments for Pool and DecPool

--- x/rewards/types/events.go
+++ x/rewards/types/events.go
@@ -13,6 +13,12 @@ const (
 	AttributeKeyDelegationType     = "delegation_type"
 	AttributeKeyDelegationTargetID = "delegation_target_id"
 
+	// AttributeKeyAmountPerPool represents the amount of rewards per pool(per
+	// denom).
+	// See https://github.com/initia-labs/initia/blob/v0.2.10/x/distribution/types/events.go#L3-L6
+	// for the reference of these attributes.
 	AttributeKeyAmountPerPool = "amount_per_pool"
-	AttributeKeyPool          = "pool" // NOTE: it's different from the restaking pool
+	// AttributeKeyPool represents teh rewards pool's name(denom). Note that
+	// the meaning of the name "pool" is different from the restaking pool.
+	AttributeKeyPool = "pool"
 )
