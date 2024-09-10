9ec2f5e149a07cd92686f5343711a08ddd587a83
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-09-10 11:41:36 -0500

    feat: add restaking migration

--- x/restaking/module.go
+++ x/restaking/module.go
@@ -23,7 +23,7 @@ import (
 )
 
 const (
-	consensusVersion = 1
+	consensusVersion = 2
 )
 
 var (
@@ -116,7 +116,14 @@ func (am AppModule) Name() string {
 
 // RegisterServices registers a GRPC query service to respond to the module-specific GRPC queries.
 func (am AppModule) RegisterServices(cfg module.Configurator) {
+	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServer(am.keeper))
 	types.RegisterQueryServer(cfg.QueryServer(), keeper.NewQuerier(am.keeper))
+
+	m := keeper.NewMigrator(am.keeper)
+	err := cfg.RegisterMigration(types.ModuleName, 1, m.Migrate1To2)
+	if err != nil {
+		panic(err)
+	}
 }
 
 // RegisterInvariants registers the restaking module's invariants.
