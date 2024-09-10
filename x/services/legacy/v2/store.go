f4def17f40071eacccf49e4a51a5ee7cefb570d7
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-09-10 11:48:29 -0500

    feat: add services migration

---
+++ x/services/legacy/v2/store.go
@@ -0,0 +1,22 @@
+package v2
+
+import (
+	storetypes "cosmossdk.io/store/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/milkyway-labs/milkyway/x/services/types"
+)
+
+// MigrateStore performs in-place store migrations from v1 to v2
+// The things done here are the following:
+// 1. setting up the module params
+func MigrateStore(ctx sdk.Context, storeKey storetypes.StoreKey, cdc codec.BinaryCodec) error {
+	store := ctx.KVStore(storeKey)
+
+	// Set the module params
+	params := types.DefaultParams()
+	store.Set(types.ParamsKey, cdc.MustMarshal(&params))
+
+	return nil
+}
