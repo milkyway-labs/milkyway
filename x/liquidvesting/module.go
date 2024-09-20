ce4ca742cf1bfd9a33d6b2369b202acebb28538f
Manuel <manuel@milkyway.zone>
2024-09-20 17:20:38 +0200

    chore: implementted modules interfaces

--- x/liquidvesting/module.go
+++ x/liquidvesting/module.go
@@ -89,6 +89,45 @@ type AppModule struct {
 	keeper *keeper.Keeper
 }
 
+func NewAppModule(cdc codec.Codec, keeper *keeper.Keeper) AppModule {
+	return AppModule{
+		AppModuleBasic: NewAppModuleBasic(cdc),
+		keeper:         keeper,
+	}
+}
+
+// Name returns the module's name.
+func (am AppModule) Name() string {
+	return am.AppModuleBasic.Name()
+}
+
+// RegisterServices registers a GRPC query service to respond to the module-specific GRPC queries.
+func (am AppModule) RegisterServices(cfg module.Configurator) {
+	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServer(am.keeper))
+	types.RegisterQueryServer(cfg.QueryServer(), keeper.NewQuerier(am.keeper))
+}
+
+// InitGenesis performs the module's genesis initialization It returns no validator updates.
+func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, gs json.RawMessage) {
+	var genState types.GenesisState
+	cdc.MustUnmarshalJSON(gs, &genState)
+
+	err := am.keeper.InitGenesis(ctx, &genState)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// ExportGenesis returns the module's exported genesis state as raw JSON bytes.
+func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.RawMessage {
+	genState, err := am.keeper.ExportGenesis(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	return cdc.MustMarshalJSON(genState)
+}
+
 // ConsensusVersion implements ConsensusVersion.
 func (AppModule) ConsensusVersion() uint64 { return consensusVersion }
 
