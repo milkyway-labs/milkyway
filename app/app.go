initia-labs/miniwasm

create db for kvindexer in AppCreator (#52)

app/app.go
@@ -278,6 +278,7 @@ type MinitiaApp struct {
 func NewMinitiaApp(
 	logger log.Logger,
 	db dbm.DB,
+	kvindexerDB dbm.DB,
 	traceStore io.Writer,
 	loadLatest bool,
 	wasmOpts []wasmkeeper.Option,
@@ -802,7 +803,7 @@ func NewMinitiaApp(
 		marketmap.NewAppModule(appCodec, app.MarketMapKeeper),
 	)
 
-	if err := app.setupIndexer(appOpts, homePath, ac, vc, appCodec); err != nil {
+	if err := app.setupIndexer(kvindexerDB, appOpts, ac, vc, appCodec); err != nil {
 		panic(err)
 	}
 
@@ -1314,7 +1315,7 @@ func (app *MinitiaApp) GetScopedIBCKeeper() capabilitykeeper.ScopedKeeper {
 func (app *MinitiaApp) TxConfig() client.TxConfig {
 	return app.txConfig
 }
-func (app *MinitiaApp) setupIndexer(appOpts servertypes.AppOptions, homePath string, ac, vc address.Codec, appCodec codec.Codec) error {
+func (app *MinitiaApp) setupIndexer(db dbm.DB, appOpts servertypes.AppOptions, ac, vc address.Codec, appCodec codec.Codec) error {
 	// initialize the indexer fake-keeper
 	indexerConfig, err := indexerconfig.NewConfig(appOpts)
 	if err != nil {
@@ -1323,7 +1324,7 @@ func (app *MinitiaApp) setupIndexer(appOpts servertypes.AppOptions, homePath str
 	app.indexerKeeper = indexerkeeper.NewKeeper(
 		appCodec,
 		"wasm",
-		homePath,
+		db,
 		indexerConfig,
 		ac,
 		vc,

app/encoding.go
@@ -23,7 +23,7 @@ import (
 
 // MakeEncodingConfig creates an EncodingConfig for testing
 func MakeEncodingConfig() params.EncodingConfig {
-	tempApp := NewMinitiaApp(log.NewNopLogger(), dbm.NewMemDB(), nil, true, []wasmkeeper.Option{}, NewEmptyAppOptions())
+	tempApp := NewMinitiaApp(log.NewNopLogger(), dbm.NewMemDB(), dbm.NewMemDB(), nil, true, []wasmkeeper.Option{}, NewEmptyAppOptions())
 	encodingConfig := params.EncodingConfig{
 		InterfaceRegistry: tempApp.InterfaceRegistry(),
 		Codec:             tempApp.AppCodec(),
@@ -35,7 +35,7 @@ func MakeEncodingConfig() params.EncodingConfig {
 }
 
 func AutoCliOpts() autocli.AppOptions {
-	tempApp := NewMinitiaApp(log.NewNopLogger(), dbm.NewMemDB(), nil, true, []wasmkeeper.Option{}, NewEmptyAppOptions())
+	tempApp := NewMinitiaApp(log.NewNopLogger(), dbm.NewMemDB(), dbm.NewMemDB(), nil, true, []wasmkeeper.Option{}, NewEmptyAppOptions())
 	modules := make(map[string]appmodule.AppModule, 0)
 	for _, m := range tempApp.ModuleManager.Modules {
 		if moduleWithName, ok := m.(module.HasName); ok {
@@ -56,7 +56,7 @@ func AutoCliOpts() autocli.AppOptions {
 }
 
 func BasicManager() module.BasicManager {
-	tempApp := NewMinitiaApp(log.NewNopLogger(), dbm.NewMemDB(), nil, true, []wasmkeeper.Option{}, NewEmptyAppOptions())
+	tempApp := NewMinitiaApp(log.NewNopLogger(), dbm.NewMemDB(), dbm.NewMemDB(), nil, true, []wasmkeeper.Option{}, NewEmptyAppOptions())
 	return tempApp.BasicModuleManager
 }
 

cmd/minitiad/root.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 	"path"
+	"path/filepath"
 
 	tmcli "github.com/cometbft/cometbft/libs/cli"
 	"golang.org/x/sync/errgroup"
@@ -47,6 +48,9 @@ import (
 	minitiaapp "github.com/initia-labs/miniwasm/app"
 
 	opchildcli "github.com/initia-labs/OPinit/x/opchild/client/cli"
+	kvindexerconfig "github.com/initia-labs/kvindexer/config"
+	kvindexerstore "github.com/initia-labs/kvindexer/store"
+	kvindexerkeeper "github.com/initia-labs/kvindexer/x/kvindexer/keeper"
 )
 
 // NewRootCmd creates a new root command for initiad. It is called once in the
@@ -267,15 +271,20 @@ func (a *appCreator) AppCreator() servertypes.AppCreator {
 			wasmOpts = append(wasmOpts, wasmkeeper.WithVMCacheMetrics(prometheus.DefaultRegisterer))
 		}
 
+		dbDir, kvdbConfig := getDBConfig(appOpts)
+		kvindexerDB, err := kvindexerstore.OpenDB(dbDir, kvindexerkeeper.StoreName, kvdbConfig.BackendConfig)
+		if err != nil {
+			panic(err)
+		}
+
 		app := minitiaapp.NewMinitiaApp(
-			logger, db, traceStore, true,
+			logger, db, kvindexerDB, traceStore, true,
 			wasmOpts,
 			appOpts,
 			baseappOptions...,
 		)
 
 		a.app = app
-
 		return app
 	}
 }
@@ -302,13 +311,13 @@ func (a appCreator) appExport(
 
 	var initiaApp *minitiaapp.MinitiaApp
 	if height != -1 {
-		initiaApp = minitiaapp.NewMinitiaApp(logger, db, traceStore, false, []wasmkeeper.Option{}, appOpts)
+		initiaApp = minitiaapp.NewMinitiaApp(logger, db, dbm.NewMemDB(), traceStore, false, []wasmkeeper.Option{}, appOpts)
 
 		if err := initiaApp.LoadHeight(height); err != nil {
 			return servertypes.ExportedApp{}, err
 		}
 	} else {
-		initiaApp = minitiaapp.NewMinitiaApp(logger, db, traceStore, true, []wasmkeeper.Option{}, appOpts)
+		initiaApp = minitiaapp.NewMinitiaApp(logger, db, dbm.NewMemDB(), traceStore, true, []wasmkeeper.Option{}, appOpts)
 	}
 
 	return initiaApp.ExportAppStateAndValidators(forZeroHeight, jailAllowedAddrs, modulesToExport)
@@ -356,4 +365,24 @@ func readEnv(clientCtx client.Context) (client.Context, error) {
 	}
 
 	return clientCtx, nil
+}
+
+// getDBConfig returns the database configuration for the EVM indexer
+func getDBConfig(appOpts servertypes.AppOptions) (string, *kvindexerconfig.IndexerConfig) {
+	rootDir := cast.ToString(appOpts.Get("home"))
+	dbDir := cast.ToString(appOpts.Get("db_dir"))
+	dbBackend, err := kvindexerconfig.NewConfig(appOpts)
+	if err != nil {
+		panic(err)
+	}
+
+	return rootify(dbDir, rootDir), dbBackend
+}
+
+// helper function to make config creation independent of root dir
+func rootify(path, root string) string {
+	if filepath.IsAbs(path) {
+		return path
+	}
+	return filepath.Join(root, path)
 }

milkyway-labs/milkyway

app/app.go
@@ -336,6 +336,7 @@ type MilkyWayApp struct {
 func NewMilkyWayApp(
 	logger log.Logger,
 	db dbm.DB,
+	kvindexerDB dbm.DB,
 	traceStore io.Writer,
 	loadLatest bool,
 	wasmOpts []wasmkeeper.Option,
@@ -1009,7 +1010,7 @@ func NewMilkyWayApp(
 		rewards.NewAppModule(appCodec, app.RewardsKeeper),
 	)
 
-	if err := app.setupIndexer(appOpts, homePath, ac, vc, appCodec); err != nil {
+	if err := app.setupIndexer(kvindexerDB, appOpts, ac, vc, appCodec); err != nil {
 		panic(err)
 	}
 
@@ -1579,7 +1580,7 @@ func (app *MilkyWayApp) GetScopedIBCKeeper() capabilitykeeper.ScopedKeeper {
 func (app *MilkyWayApp) TxConfig() client.TxConfig {
 	return app.txConfig
 }
-func (app *MilkyWayApp) setupIndexer(appOpts servertypes.AppOptions, homePath string, ac, vc address.Codec, appCodec codec.Codec) error {
+func (app *MilkyWayApp) setupIndexer(db dbm.DB, appOpts servertypes.AppOptions, ac, vc address.Codec, appCodec codec.Codec) error {
 	// initialize the indexer fake-keeper
 	indexerConfig, err := indexerconfig.NewConfig(appOpts)
 	if err != nil {
@@ -1588,7 +1589,7 @@ func (app *MilkyWayApp) setupIndexer(appOpts servertypes.AppOptions, homePath st
 	app.indexerKeeper = indexerkeeper.NewKeeper(
 		appCodec,
 		"wasm",
-		homePath,
+		db,
 		indexerConfig,
 		ac,
 		vc,

app/encoding.go
@@ -30,7 +30,7 @@ func MakeCodecs() (codec.Codec, *codec.LegacyAmino) {
 
 // MakeEncodingConfig creates an EncodingConfig for testing
 func MakeEncodingConfig() params.EncodingConfig {
-	tempApp := NewMilkyWayApp(log.NewNopLogger(), dbm.NewMemDB(), nil, true, []wasmkeeper.Option{}, NewEmptyAppOptions())
+	tempApp := NewMilkyWayApp(log.NewNopLogger(), dbm.NewMemDB(), dbm.NewMemDB(), nil, true, []wasmkeeper.Option{}, NewEmptyAppOptions())
 	encodingConfig := params.EncodingConfig{
 		InterfaceRegistry: tempApp.InterfaceRegistry(),
 		Codec:             tempApp.AppCodec(),
@@ -42,7 +42,7 @@ func MakeEncodingConfig() params.EncodingConfig {
 }
 
 func AutoCliOpts() autocli.AppOptions {
-	tempApp := NewMilkyWayApp(log.NewNopLogger(), dbm.NewMemDB(), nil, true, []wasmkeeper.Option{}, NewEmptyAppOptions())
+	tempApp := NewMilkyWayApp(log.NewNopLogger(), dbm.NewMemDB(), dbm.NewMemDB(), nil, true, []wasmkeeper.Option{}, NewEmptyAppOptions())
 	modules := make(map[string]appmodule.AppModule, 0)
 	for _, m := range tempApp.ModuleManager.Modules {
 		if moduleWithName, ok := m.(module.HasName); ok {
@@ -63,7 +63,7 @@ func AutoCliOpts() autocli.AppOptions {
 }
 
 func BasicManager() module.BasicManager {
-	tempApp := NewMilkyWayApp(log.NewNopLogger(), dbm.NewMemDB(), nil, true, []wasmkeeper.Option{}, NewEmptyAppOptions())
+	tempApp := NewMilkyWayApp(log.NewNopLogger(), dbm.NewMemDB(), dbm.NewMemDB(), nil, true, []wasmkeeper.Option{}, NewEmptyAppOptions())
 	return tempApp.BasicModuleManager
 }
 

cmd/milkywayd/root.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 	"path"
+	"path/filepath"
 
 	tmcli "github.com/cometbft/cometbft/libs/cli"
 	"golang.org/x/sync/errgroup"
@@ -45,6 +46,9 @@ import (
 
 	opchildcli "github.com/initia-labs/OPinit/x/opchild/client/cli"
 	"github.com/initia-labs/initia/app/params"
+	kvindexerconfig "github.com/initia-labs/kvindexer/config"
+	kvindexerstore "github.com/initia-labs/kvindexer/store"
+	kvindexerkeeper "github.com/initia-labs/kvindexer/x/kvindexer/keeper"
 
 	milkywayapp "github.com/milkyway-labs/milkyway/app"
 )
@@ -267,15 +271,20 @@ func (a *appCreator) AppCreator() servertypes.AppCreator {
 			wasmOpts = append(wasmOpts, wasmkeeper.WithVMCacheMetrics(prometheus.DefaultRegisterer))
 		}
 
+		dbDir, kvdbConfig := getDBConfig(appOpts)
+		kvindexerDB, err := kvindexerstore.OpenDB(dbDir, kvindexerkeeper.StoreName, kvdbConfig.BackendConfig)
+		if err != nil {
+			panic(err)
+		}
+
 		app := milkywayapp.NewMilkyWayApp(
-			logger, db, traceStore, true,
+			logger, db, kvindexerDB, traceStore, true,
 			wasmOpts,
 			appOpts,
 			baseappOptions...,
 		)
 
 		a.app = app
-
 		return app
 	}
 }
@@ -302,13 +311,13 @@ func (a appCreator) appExport(
 
 	var initiaApp *milkywayapp.MilkyWayApp
 	if height != -1 {
-		initiaApp = milkywayapp.NewMilkyWayApp(logger, db, traceStore, false, []wasmkeeper.Option{}, appOpts)
+		initiaApp = milkywayapp.NewMilkyWayApp(logger, db, dbm.NewMemDB(), traceStore, false, []wasmkeeper.Option{}, appOpts)
 
 		if err := initiaApp.LoadHeight(height); err != nil {
 			return servertypes.ExportedApp{}, err
 		}
 	} else {
-		initiaApp = milkywayapp.NewMilkyWayApp(logger, db, traceStore, true, []wasmkeeper.Option{}, appOpts)
+		initiaApp = milkywayapp.NewMilkyWayApp(logger, db, dbm.NewMemDB(), traceStore, true, []wasmkeeper.Option{}, appOpts)
 	}
 
 	return initiaApp.ExportAppStateAndValidators(forZeroHeight, jailAllowedAddrs, modulesToExport)
@@ -356,4 +365,24 @@ func readEnv(clientCtx client.Context) (client.Context, error) {
 	}
 
 	return clientCtx, nil
+}
+
+// getDBConfig returns the database configuration for the EVM indexer
+func getDBConfig(appOpts servertypes.AppOptions) (string, *kvindexerconfig.IndexerConfig) {
+	rootDir := cast.ToString(appOpts.Get("home"))
+	dbDir := cast.ToString(appOpts.Get("db_dir"))
+	dbBackend, err := kvindexerconfig.NewConfig(appOpts)
+	if err != nil {
+		panic(err)
+	}
+
+	return rootify(dbDir, rootDir), dbBackend
+}
+
+// helper function to make config creation independent of root dir
+func rootify(path, root string) string {
+	if filepath.IsAbs(path) {
+		return path
+	}
+	return filepath.Join(root, path)
 }