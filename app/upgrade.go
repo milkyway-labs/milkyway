e2cb502af1caa2be04b459457a9cf748906a7798
Hanjun Kim <hallazzang@gmail.com>
2024-10-31 13:55:17 +0800

    feat: v1.2.2 upgrade (#140)
    ## Description
    
    This upgrade fixes the host zone record's unbonding period(from 7 to 21
    days) for Initia testnet
    
    Closes: #XXXX
    
    <!-- Add a description of the changes that this PR introduces and the
    files that
    are the most critical to review. -->
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is
    not applicable and
    please add links to any relevant follow up issues.*
    
    I have...
    
    - [ ] included the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] added `!` to the type prefix if API or client breaking change
    - [ ] targeted the correct branch (see [PR
    Targeting](https://github.com/milkyway-labs/milkyway/blob/master/CONTRIBUTING.md#pr-targeting))
    - [ ] provided a link to the relevant issue or specification
    - [ ] followed the guidelines for [building
    modules](https://docs.cosmos.network/v0.44/building-modules/intro.html)
    - [ ] included the necessary unit and integration
    [tests](https://github.com/milkyway-labs/milkyway/blob/master/CONTRIBUTING.md#testing)
    - [ ] added a changelog entry to `CHANGELOG.md`
    - [ ] included comments for [documenting Go
    code](https://blog.golang.org/godoc)
    - [ ] updated the relevant documentation or specification
    - [ ] reviewed "Files changed" and left comments if necessary
    - [ ] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable
    and please add
    your handle next to the items reviewed if you only reviewed selected
    items.*
    
    I have...
    
    - [ ] confirmed the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] confirmed `!` in the type prefix if API or client breaking change
    - [ ] confirmed all author checklist items have been addressed
    - [ ] reviewed state machine logic
    - [ ] reviewed API design and naming
    - [ ] reviewed documentation is accurate
    - [ ] reviewed tests and test coverage
    - [ ] manually tested (if applicable)

--- app/upgrade.go
+++ app/upgrade.go
@@ -2,9 +2,11 @@ package app
 
 import (
 	v110 "github.com/milkyway-labs/milkyway/app/upgrades/v110"
+	v122 "github.com/milkyway-labs/milkyway/app/upgrades/v122"
 )
 
 // RegisterUpgradeHandlers returns upgrade handlers
 func (app *MilkyWayApp) RegisterUpgradeHandlers() {
 	app.registerUpgrade(v110.NewUpgrade(app.ModuleManager, app.Configurator(), app.appCodec, app.keys, app.RewardsKeeper))
+	app.registerUpgrade(v122.NewUpgrade(app.ModuleManager, app.Configurator(), app.StakeIBCKeeper))
 }
