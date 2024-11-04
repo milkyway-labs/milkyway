980459573f80a7c8391ea30d5a0ecf771bb24093
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-11-04 15:54:24 +0900

    fix: remove x/liquidvesting from v1.3.0 upgrade handler

--- app/upgrades/v130/upgrade.go
+++ app/upgrades/v130/upgrade.go
@@ -5,12 +5,10 @@ import (
 
 	storetypes "cosmossdk.io/store/types"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 
 	"github.com/milkyway-labs/milkyway/app/upgrades"
 	liquidvestingkeeper "github.com/milkyway-labs/milkyway/x/liquidvesting/keeper"
-	liquidvestingtypes "github.com/milkyway-labs/milkyway/x/liquidvesting/types"
 )
 
 var (
@@ -47,15 +45,6 @@ func (u *Upgrade) Name() string {
 // Handler implements upgrades.Upgrade
 func (u *Upgrade) Handler() upgradetypes.UpgradeHandler {
 	return func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		sdkCtx := sdk.UnwrapSDKContext(ctx)
-
-		// Set up the new x/liquidvesting module
-		err := u.liquidVestingKeeper.SetParams(sdkCtx, liquidvestingtypes.DefaultParams())
-		if err != nil {
-			return nil, err
-		}
-		fromVM[liquidvestingtypes.ModuleName] = 1
-
 		// This upgrade does not require any migration, so we can simply return the current version map
 		return u.mm.RunMigrations(ctx, u.configurator, fromVM)
 	}
@@ -64,9 +53,7 @@ func (u *Upgrade) Handler() upgradetypes.UpgradeHandler {
 // StoreUpgrades implements upgrades.Upgrade
 func (u *Upgrade) StoreUpgrades() *storetypes.StoreUpgrades {
 	return &storetypes.StoreUpgrades{
-		Added: []string{
-			liquidvestingtypes.ModuleName,
-		},
+		Added:   nil,
 		Renamed: nil,
 		Deleted: nil,
 	}
