1572cd7298b06bc517e2ea47542c22948e48da8c
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-11-04 12:04:30 +0900

    feat: add v1.3.0 upgrade registration

--- app/upgrade.go
+++ app/upgrade.go
@@ -3,10 +3,12 @@ package app
 import (
 	v110 "github.com/milkyway-labs/milkyway/app/upgrades/v110"
 	v122 "github.com/milkyway-labs/milkyway/app/upgrades/v122"
+	v130 "github.com/milkyway-labs/milkyway/app/upgrades/v130"
 )
 
 // RegisterUpgradeHandlers returns upgrade handlers
 func (app *MilkyWayApp) RegisterUpgradeHandlers() {
 	app.registerUpgrade(v110.NewUpgrade(app.ModuleManager, app.Configurator(), app.appCodec, app.keys, app.RewardsKeeper))
 	app.registerUpgrade(v122.NewUpgrade(app.ModuleManager, app.Configurator(), app.StakeIBCKeeper))
+	app.registerUpgrade(v130.NewUpgrade(app.ModuleManager, app.Configurator(), app.LiquidVestingKeeper))
 }
