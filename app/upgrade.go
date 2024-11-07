7031282dfef2d5a51a7f3dd47ccb0d8bf792556f
Manuel <6245917+manu0466@users.noreply.github.com>
2024-11-07 19:56:46 +0900

    feat: add v1.4.0 upgrade handler

--- app/upgrade.go
+++ app/upgrade.go
@@ -4,6 +4,7 @@ import (
 	v110 "github.com/milkyway-labs/milkyway/app/upgrades/v110"
 	v122 "github.com/milkyway-labs/milkyway/app/upgrades/v122"
 	v130 "github.com/milkyway-labs/milkyway/app/upgrades/v130"
+	v140 "github.com/milkyway-labs/milkyway/app/upgrades/v140"
 )
 
 // RegisterUpgradeHandlers returns upgrade handlers
@@ -11,4 +12,5 @@ func (app *MilkyWayApp) RegisterUpgradeHandlers() {
 	app.registerUpgrade(v110.NewUpgrade(app.ModuleManager, app.Configurator(), app.appCodec, app.keys, app.RewardsKeeper))
 	app.registerUpgrade(v122.NewUpgrade(app.ModuleManager, app.Configurator(), app.StakeIBCKeeper))
 	app.registerUpgrade(v130.NewUpgrade(app.ModuleManager, app.Configurator(), app.LiquidVestingKeeper))
+	app.registerUpgrade(v140.NewUpgrade(app.ModuleManager, app.Configurator(), app.LiquidVestingKeeper))
 }
