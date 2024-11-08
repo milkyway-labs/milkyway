7246df3cee2f9ecff61e0be0c4ed30c67ba718d3
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-11-08 16:14:51 +0900

    refactor(tests): improve operators tests

---
+++ x/operators/testutils/keeper.go
@@ -0,0 +1,53 @@
+package testutils
+
+import (
+	"testing"
+
+	storetypes "cosmossdk.io/store/types"
+	"github.com/cosmos/cosmos-sdk/runtime"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+
+	"github.com/milkyway-labs/milkyway/app/keepers"
+	"github.com/milkyway-labs/milkyway/testutils/storetesting"
+	"github.com/milkyway-labs/milkyway/x/operators/keeper"
+	"github.com/milkyway-labs/milkyway/x/operators/types"
+)
+
+type KeeperTestData struct {
+	storetesting.BaseKeeperTestData
+
+	StoreKey storetypes.StoreKey
+	Keeper   *keeper.Keeper
+
+	Hooks *MockHooks
+}
+
+func NewKeeperTestData(t *testing.T) KeeperTestData {
+	t.Helper()
+
+	var data = KeeperTestData{
+		BaseKeeperTestData: storetesting.NewBaseKeeperTestData(t, []string{
+			types.StoreKey, authtypes.StoreKey, banktypes.StoreKey,
+		}),
+	}
+
+	// Set the store key
+	data.StoreKey = data.Keys[types.StoreKey]
+
+	// Build keepers
+	data.Keeper = keeper.NewKeeper(
+		data.Cdc,
+		data.StoreKey,
+		runtime.NewKVStoreService(data.Keys[types.StoreKey]),
+		data.AccountKeeper,
+		keepers.NewCommunityPoolKeeper(data.BankKeeper, authtypes.FeeCollectorName),
+		data.AuthorityAddress,
+	)
+
+	// Setup the hooks
+	data.Hooks = NewMockHooks()
+	data.Keeper = data.Keeper.SetHooks(data.Hooks)
+
+	return data
+}
