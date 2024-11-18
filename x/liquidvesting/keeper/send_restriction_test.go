347b668ed9928bbad3df751ec595a9054505e797
Manuel <6245917+manu0466@users.noreply.github.com>
2024-11-19 04:12:54 +0900

    feat(tests): add tests for PR #175

--- x/liquidvesting/keeper/send_restriction_test.go
+++ x/liquidvesting/keeper/send_restriction_test.go
@@ -2,9 +2,14 @@ package keeper_test
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 
+	liquidvestingtypes "github.com/milkyway-labs/milkyway/x/liquidvesting/types"
+	operatorstypes "github.com/milkyway-labs/milkyway/x/operators/types"
+	poolstypes "github.com/milkyway-labs/milkyway/x/pools/types"
 	restakingkeeper "github.com/milkyway-labs/milkyway/x/restaking/keeper"
 	restakingtypes "github.com/milkyway-labs/milkyway/x/restaking/types"
+	servicestypes "github.com/milkyway-labs/milkyway/x/services/types"
 )
 
 const (
@@ -363,6 +368,106 @@ func (suite *KeeperTestSuite) TestBankHooks_TestOperatorRestaking() {
 				insuranceFund, err := suite.k.GetUserInsuranceFund(suite.ctx, sdk.MustAccAddressFromBech32(restaker))
 				suite.Assert().NoError(err)
 				suite.Assert().Equal(tc.expectedUsage, insuranceFund.Used)
+			}
+		})
+	}
+}
+
+func (suite *KeeperTestSuite) TestKeeper_SendRegistrionFn() {
+	testCase := []struct {
+		name      string
+		store     func(ctx sdk.Context)
+		from      string
+		to        string
+		amount    sdk.Coins
+		shouldErr bool
+		expTo     string
+	}{
+		{
+			name:      "sending normal coins from user to user works properly",
+			from:      "cosmos1pgzph9rze2j2xxavx4n7pdhxlkgsq7raqh8hre",
+			to:        "cosmos167x6ehhple8gwz5ezy9x0464jltvdpzl6qfdt4",
+			amount:    sdk.NewCoins(sdk.NewInt64Coin("ibc/1", 100)),
+			shouldErr: false,
+			expTo:     "cosmos167x6ehhple8gwz5ezy9x0464jltvdpzl6qfdt4",
+		},
+		{
+			name:      "sending vested representation from user to user returns error",
+			from:      "cosmos1pgzph9rze2j2xxavx4n7pdhxlkgsq7raqh8hre",
+			to:        "cosmos167x6ehhple8gwz5ezy9x0464jltvdpzl6qfdt4",
+			amount:    sdk.NewCoins(sdk.NewInt64Coin("vested/stake", 100)),
+			shouldErr: true,
+		},
+		{
+			name: "sending normal coins between restaking targets is not allowed",
+			store: func(ctx sdk.Context) {
+				// Create a test service and operator
+				suite.createService(testServiceId)
+				suite.createOperator(testOperatorId)
+			},
+			from:      servicestypes.GetServiceAddress(testServiceId).String(),
+			to:        operatorstypes.GetOperatorAddress(testOperatorId).String(),
+			amount:    sdk.NewCoins(sdk.NewInt64Coin("stake", 100)),
+			shouldErr: true,
+		},
+		{
+			name: "sending normal coins between restaking targets is not allowed",
+			store: func(ctx sdk.Context) {
+				// Create a test service and operator
+				suite.createPool(testPoolId, vestedIBCDenom)
+				suite.createOperator(testOperatorId)
+			},
+			from:      poolstypes.GetPoolAddress(testPoolId).String(),
+			to:        operatorstypes.GetOperatorAddress(testOperatorId).String(),
+			amount:    sdk.NewCoins(sdk.NewInt64Coin("vested/stake", 100)),
+			shouldErr: true,
+		},
+		{
+			name: "sending coins from the module account is allowed",
+			from: authtypes.NewModuleAddress(liquidvestingtypes.ModuleName).String(),
+			to:   "cosmos167x6ehhple8gwz5ezy9x0464jltvdpzl6qfdt4",
+			amount: sdk.NewCoins(
+				sdk.NewInt64Coin("ibc/1", 100),
+				sdk.NewInt64Coin("vested/stake", 200),
+			),
+			shouldErr: false,
+			expTo:     "cosmos167x6ehhple8gwz5ezy9x0464jltvdpzl6qfdt4",
+		},
+		{
+			name: "sending coins to the module account is allowed",
+			from: "cosmos167x6ehhple8gwz5ezy9x0464jltvdpzl6qfdt4",
+			to:   authtypes.NewModuleAddress(liquidvestingtypes.ModuleName).String(),
+			amount: sdk.NewCoins(
+				sdk.NewInt64Coin("ibc/1", 100),
+				sdk.NewInt64Coin("vested/stake", 200),
+			),
+			shouldErr: false,
+			expTo:     authtypes.NewModuleAddress(liquidvestingtypes.ModuleName).String(),
+		},
+	}
+
+	for _, tc := range testCase {
+		tc := tc
+		suite.Run(tc.name, func() {
+			suite.SetupTest()
+
+			ctx, _ := suite.ctx.CacheContext()
+			if tc.store != nil {
+				tc.store(ctx)
+			}
+
+			from, err := sdk.AccAddressFromBech32(tc.from)
+			suite.Require().NoError(err)
+
+			to, err := sdk.AccAddressFromBech32(tc.to)
+			suite.Require().NoError(err)
+
+			receivedTo, err := suite.k.SendRestrictionFn(ctx, from, to, tc.amount)
+			if tc.shouldErr {
+				suite.Require().Error(err)
+			} else {
+				suite.Require().NoError(err)
+				suite.Require().Equal(tc.expTo, receivedTo.String())
 			}
 		})
 	}
