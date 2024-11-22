bfc266faa2bb54647c5af4eaf85992b16a78eb05
Hanjun Kim <hallazzang@gmail.com>
2024-11-22 18:06:10 +0900

    fix: remove kvindexer dependency

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -9,7 +9,6 @@ IBC_V=v8
 INITIA_URL=github.com/initia-labs/initia
 OPINIT_URL=github.com/initia-labs/OPinit
 COSMWASM_URL=github.com/CosmWasm/wasmd
-INDEXER_URL=github.com/initia-labs/kvindexer
 CONNECT_URL=github.com/skip-mev/connect
 CONNECT_V=v2
 
@@ -18,7 +17,6 @@ IBC_VERSION=$(cat ./go.mod | grep "$IBC_URL/$IBC_V v" | sed -n -e "s/^.* //p")
 COSMWASM_VERSION=$(cat ./go.mod | grep "$COSMWASM_URL v" | sed -n -e "s/^.* //p")
 INITIA_VERSION=$(cat ./go.mod | grep "$INITIA_URL v" | sed -n -e "s/^.* //p")
 OPINIT_VERSION=$(cat ./go.mod | grep "$OPINIT_URL v" | sed -n -e "s/^.* //p")
-INDEXER_VERSION=$(cat ./go.mod | grep "$INDEXER_URL v" | sed -n -e "s/^.* //p")
 CONNECT_VERSION=$(cat ./go.mod | grep "$CONNECT_URL/$CONNECT_V v" | sed -n -e "s/^.* //p")
 
 mkdir -p ./third_party
@@ -28,7 +26,6 @@ git clone -b $OPINIT_VERSION https://$OPINIT_URL
 git clone -b $COSMOS_SDK_VERSION https://$COSMOS_URL
 git clone -b $IBC_VERSION https://$IBC_URL
 git clone -b $COSMWASM_VERSION https://$COSMWASM_URL
-git clone -b $INDEXER_VERSION https://$INDEXER_URL
 git clone -b $CONNECT_VERSION https://$CONNECT_URL
 cd ..
 
@@ -43,7 +40,6 @@ proto_dirs=$(find \
   ../third_party/initia/proto \
   ../third_party/opinit/proto \
   ../third_party/wasmd/proto \
-  ../third_party/kvindexer/proto \
   ../third_party/connect/proto \
   -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
