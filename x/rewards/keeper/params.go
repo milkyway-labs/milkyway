86ac7c34f9e8d8e514081707ed81694b0257c21d
Hanjun Kim <hallazzang@gmail.com>
2024-12-06 18:21:48 +0900

    feat: add GetParams and SetParams methods

---
+++ x/rewards/keeper/params.go
@@ -0,0 +1,27 @@
+package keeper
+
+import (
+	"context"
+	"errors"
+
+	"cosmossdk.io/collections"
+
+	"github.com/milkyway-labs/milkyway/v3/x/rewards/types"
+)
+
+// SetParams sets module parameters
+func (k *Keeper) SetParams(ctx context.Context, params types.Params) error {
+	return k.Params.Set(ctx, params)
+}
+
+// GetParams returns the module parameters
+func (k *Keeper) GetParams(ctx context.Context) (p types.Params, err error) {
+	p, err = k.Params.Get(ctx)
+	if err != nil {
+		if errors.Is(err, collections.ErrNotFound) {
+			return types.DefaultParams(), nil
+		}
+		return types.Params{}, err
+	}
+	return p, nil
+}
