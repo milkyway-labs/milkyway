e5e618ef7bd4199eeea513481d8ff0ce636951ea
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-12-17 05:34:09 +0900

    chore: update v6 upgrade handler for v6.1.0

--- app/upgrades/v6/upgrades.go
+++ app/upgrades/v6/upgrades.go
@@ -2,12 +2,8 @@ package v6
 
 import (
 	"context"
-	"maps"
-	"slices"
-	"strings"
 
 	upgradetypes "cosmossdk.io/x/upgrade/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 
 	"github.com/milkyway-labs/milkyway/v6/app/keepers"
@@ -23,33 +19,6 @@ func CreateUpgradeHandler(
 		vm, err := mm.RunMigrations(ctx, configuration, fromVM)
 		if err != nil {
 			return nil, err
-		}
-
-		// Unwrap the context
-		sdkCtx := sdk.UnwrapSDKContext(ctx)
-
-		// Remove all the markets except from the TIA/USD market
-		markets, err := keepers.MarketMapKeeper.GetAllMarkets(sdkCtx)
-		if err != nil {
-			return nil, err
-		}
-
-		for _, ticker := range slices.Sorted(maps.Keys(markets)) {
-			market := markets[ticker]
-
-			if strings.Contains(ticker, "TIA") {
-				continue
-			}
-
-			err = keepers.MarketMapKeeper.DeleteMarket(sdkCtx, ticker)
-			if err != nil {
-				return nil, err
-			}
-
-			err = keepers.OracleKeeper.RemoveCurrencyPair(sdkCtx, market.Ticker.CurrencyPair)
-			if err != nil {
-				return nil, err
-			}
 		}
 
 		keepers.AccountKeeper.GetModuleAccount(ctx, liquidvestingtypes.ModuleName)
