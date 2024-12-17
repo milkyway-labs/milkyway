b785a5101a240b9c0a8822f24c5a00dd068869d4
Riccardo <9119016+RiccardoM@users.noreply.github.com>
2024-12-17 12:31:01 +0900

    chore: add v4 upgrade handler (#222)
    ## Description
    
    This PR adds the `v4` upgrade handler that takes care of removing all
    markets and currency pairs from the `x/marketmap` and `x/oracle` modules
    except the ones that are related to `TIA` .
    
    This was done in order to reduce the amount of data stored per each
    block, since we do not use any currency pair outside of `TIA/USD` and
    its derivates inside our modules.
    
    Closes: MILK-179
    
    <!-- Add a description of the changes that this PR introduces and the
    files that
    are the most critical to review. -->
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is
    not applicable and
    please add links to any relevant follow up issues.*
    
    I have...
    
    - [x] included the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] added `!` to the type prefix if API or client breaking change
    - [x] targeted the correct branch (see [PR
    Targeting](https://github.com/milkyway-labs/milkyway/blob/master/CONTRIBUTING.md#pr-targeting))
    - [x] provided a link to the relevant issue or specification
    - [x] followed the guidelines for [building
    modules](https://docs.cosmos.network/v0.44/building-modules/intro.html)
    - [x] included the necessary unit and integration
    [tests](https://github.com/milkyway-labs/milkyway/blob/master/CONTRIBUTING.md#testing)
    - [x] added a changelog entry to `CHANGELOG.md`
    - [x] included comments for [documenting Go
    code](https://blog.golang.org/godoc)
    - [ ] updated the relevant documentation or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [x] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable
    and please add
    your handle next to the items reviewed if you only reviewed selected
    items.*
    
    I have...
    
    - [ ] confirmed the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] confirmed `!` in the type prefix if API or client breaking change
    - [ ] confirmed all author checklist items have been addressed
    - [ ] reviewed state machine logic
    - [ ] reviewed API design and naming
    - [ ] reviewed documentation is accurate
    - [ ] reviewed tests and test coverage
    - [ ] manually tested (if applicable)

---
+++ app/upgrades/v4/upgrades.go
@@ -0,0 +1,56 @@
+package v4
+
+import (
+	"context"
+	"maps"
+	"slices"
+	"strings"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	"github.com/milkyway-labs/milkyway/v3/app/keepers"
+)
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configuration module.Configurator,
+	keepers *keepers.AppKeepers,
+) upgradetypes.UpgradeHandler {
+	return func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		vm, err := mm.RunMigrations(ctx, configuration, fromVM)
+		if err != nil {
+			return nil, err
+		}
+
+		// Unwrap the context
+		sdkCtx := sdk.UnwrapSDKContext(ctx)
+
+		// Remove all the markets except from the TIA/USD market
+		markets, err := keepers.MarketMapKeeper.GetAllMarkets(sdkCtx)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, ticker := range slices.Sorted(maps.Keys(markets)) {
+			market := markets[ticker]
+
+			if strings.Contains(ticker, "TIA") {
+				continue
+			}
+
+			err = keepers.MarketMapKeeper.DeleteMarket(sdkCtx, ticker)
+			if err != nil {
+				return nil, err
+			}
+
+			err = keepers.OracleKeeper.RemoveCurrencyPair(sdkCtx, market.Ticker.CurrencyPair)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		return vm, nil
+	}
+}
