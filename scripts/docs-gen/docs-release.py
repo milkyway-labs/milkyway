af2917781364f43677e83fbae675f2c515f48b0b
Manuel <manuel@milkyway.zone>
2025-04-02 11:38:53 +0200

    feat: add docs release script

---
+++ scripts/docs-gen/docs-release.py
@@ -0,0 +1,71 @@
+import os
+import shutil
+import argparse
+from packaging.version import Version
+from utils import generate_docs
+
+
+def generate_release_docs(
+        modules_dir: str,
+        docs_dir: str,
+        release_version: str,
+):
+    print(f"Modules directory: {modules_dir}")
+    print(f"Docs directory: {docs_dir}")
+    print(f"Preparing documentation: {release_version}")
+
+    # Remove the old documentation
+    version_dir = os.path.join(docs_dir, release_version)
+    if os.path.exists(version_dir):
+        shutil.rmtree(version_dir)
+
+    # Generate the documentation
+    generate_docs(modules_dir, version_dir)
+
+    # Function to filter out the version dirs
+    def version_dirs_filter(dir_name: str) -> bool:
+        if dir_name == "main":
+            return False
+        dir_path = os.path.join(docs_dir, dir_name)
+        return os.path.isdir(dir_path)
+    # Get the various versions
+    versions = list(filter(version_dirs_filter, os.listdir(docs_dir)))
+    # Sort the versions in descending order
+    versions.sort(key=Version, reverse=True)
+
+    # Remove the old README.md
+    modules_readme = os.path.join(docs_dir, "README.md")
+    if os.path.exists(modules_readme):
+        os.remove(modules_readme)
+
+    # Generate the new README.md
+    with open(modules_readme, "w") as readme:
+        readme.write("# Modules version\n\n")
+
+        # Print first the main version
+        if os.path.exists(os.path.join(docs_dir, "main")):
+            readme.write("- [main](main/README.md)\n")
+        # Print the various versions
+        for version in versions:
+            readme.write(f"- [{version}]({version}/README.md)\n")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Generate the documentation to be published")
+    parser.add_argument("modules", help="The modules directory")
+    args = parser.parse_args()
+
+    docs_dir = os.getenv("DOCS_DIR")
+    if docs_dir is None:
+        docs_dir = "./test"
+
+    release_version = os.getenv("RELEASE_VERSION")
+    if release_version is None:
+        release_version = "main"
+
+    generate_release_docs(args.modules, docs_dir, release_version)
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/docs-gen/utils.py
@@ -0,0 +1,96 @@
+import requests
+import os
+import sys
+import re
+
+
+def github_url_to_raw(url: str) -> str:
+    return url.replace("github.com", "raw.githubusercontent.com").replace("/blob/", "/")
+
+
+def get_lines(text: str, start: int, end: int):
+    lines = text.split('\n')  # Split the string into lines
+    return '\n'.join(lines[start-1:end])
+
+
+def process_markdown_file(file_path: str, output_dir: str) -> None:
+    print(f"Processing {file_path}")
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    # Regular expression pattern for matching the section
+    pattern = r'\`\`\`\w+ reference\n((?:https://)?github\.com/.*?)\n\`\`\`'
+    references = re.findall(pattern, content)
+
+    for reference_url in set(references):  # Use set to remove duplicates
+        values = reference_url.split('#L')
+        if len(values) != 2:
+            print(f"Invalid link: {reference_url} in file ${file_path}")
+            continue
+
+        # Get the url and the lines
+        url, lines = values
+        start_end_line = lines.replace('L', '').split('-')
+        if len(start_end_line) != 2:
+            print(f"Invalid line format: {lines} in file ${file_path}")
+            continue
+        start, end = start_end_line
+
+        # Parse the lines
+        start = int(start)
+        end = int(end)
+        if start < 0 or end < 0 or start >= end:
+            print(f"Invalid line range: {start}-{end} in file ${file_path}")
+            continue
+        # Convert the reference url to a raw github url
+        raw_url = github_url_to_raw(url)
+
+        # Extract the section from the remote reference
+        res = requests.get(raw_url, allow_redirects=True)
+        reference_text = get_lines(res.text, start, end)
+
+        # Update the content with the reference
+        content = content.replace(
+            f" reference\n{reference_url}", f"\n{reference_text}")
+
+    # Save the file
+    parent = os.path.dirname(output_dir)
+    if not os.path.exists(parent):
+        os.makedirs(parent)
+
+    with open(output_dir, 'w') as file:
+        file.write(content)
+    print(f"Saved to {output_dir}")
+
+
+def generate_docs(modules_dir: str, output_dir: str) -> None:
+    # Check if the modules directory exists
+    if not os.path.isdir(modules_dir):
+        sys.exit('Error: The modules directory does not exist.')
+
+    # If output directory does not exist, create it
+    if not os.path.exists(output_dir) or not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+
+    # Recursively search for markdown files in the modules directory
+    for foldername, subfolders, filenames in os.walk(modules_dir):
+        for filename in filenames:
+            if filename.endswith('.md'):
+                file_path = os.path.join(foldername, filename)
+                output_path = file_path.replace(modules_dir, output_dir)
+                process_markdown_file(file_path, output_path)
+
+    # Delete the README.md file if it exists
+    readme_file = os.path.join(output_dir, 'README.md')
+    if os.path.exists(readme_file):
+        os.remove(readme_file)
+
+    # Prepare the list of modules
+    modules = os.listdir(output_dir)
+    modules.sort()
+
+    # Generate the README.md file
+    with open(readme_file, 'w') as readme:
+        readme.write("# Modules\n\n")
+        for module in modules:
+            readme.write(f"- [{module}]({module}/README.md)\n")
