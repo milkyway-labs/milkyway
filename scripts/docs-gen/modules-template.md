dd402f58c2176798c12d31490b31aa13769de569
Manuel <6245917+manu0466@users.noreply.github.com>
2025-04-07 16:09:38 +0200

    ci: GitBook documentation workflow (#274)
    ## Description
    
    Closes: MILK-172
    
    This PR adds a new github workflow that generates and publish the
    modules documentation to GitBook.
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is
    not applicable and
    please add links to any relevant follow up issues.*
    
    I have...
    
    - [ ] included the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] added `!` to the type prefix if API or client breaking change
    - [ ] targeted the correct branch (see [PR
    Targeting](https://github.com/milkyway-labs/milkyway/blob/master/CONTRIBUTING.md#pr-targeting))
    - [ ] provided a link to the relevant issue or specification
    - [ ] followed the guidelines for [building
    modules](https://docs.cosmos.network/v0.44/building-modules/intro.html)
    - [ ] included the necessary unit and integration
    [tests](https://github.com/milkyway-labs/milkyway/blob/master/CONTRIBUTING.md#testing)
    - [ ] added a changelog entry to `CHANGELOG.md`
    - [ ] included comments for [documenting Go
    code](https://blog.golang.org/godoc)
    - [ ] updated the relevant documentation or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [ ] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable
    and please add
    your handle next to the items reviewed if you only reviewed selected
    items.*
    
    I have...
    
    - [ ] confirmed the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] confirmed `!` in the type prefix if API or client breaking change
    - [ ] confirmed all author checklist items have been addressed
    - [ ] reviewed state machine logic
    - [ ] reviewed API design and naming
    - [ ] reviewed documentation is accurate
    - [ ] reviewed tests and test coverage
    - [ ] manually tested (if applicable)
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced an automated process that updates and publishes
    documentation when changes occur.
    - Added a new release documentation tool that generates updated
    summaries and module overviews.
      - New Markdown template created for documenting chain modules.
    
    - **Refactor**
    - Streamlined the documentation generation workflow for improved
    efficiency and reliability.
    
    - **Chores**
    - Enhanced build configurations and dependency management to support the
    updated documentation process.
    - Added new targets in the Makefile for documentation installation and
    release generation.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ scripts/docs-gen/modules-template.md
@@ -0,0 +1,20 @@
+---
+layout:
+  title:
+    visible: true
+  description:
+    visible: false
+  tableOfContents:
+    visible: true
+  outline:
+    visible: true
+  pagination:
+    visible: true
+---
+
+# Chain modules
+
+Here is the list of custom modules present in our chain:
+
+{{ modules }}
+
---
+++ scripts/docs-gen/utils.py
@@ -0,0 +1,110 @@
+import requests
+import os
+import sys
+import re
+import io
+
+
+def github_url_to_raw(url: str) -> str:
+    return url.replace("github.com", "raw.githubusercontent.com").replace("/blob/", "/")
+
+
+def get_lines(text: str, start: int, end: int):
+    lines = text.split('\n')  # Split the string into lines
+    return '\n'.join(lines[start-1:end])
+
+
+def write_gitbook_meta(file: io.TextIOWrapper):
+    file.write("""---
+layout:
+  title:
+    visible: true
+  description:
+    visible: false
+  tableOfContents:
+    visible: true
+  outline:
+    visible: false
+  pagination:
+    visible: true
+---
+
+""")
+
+
+def process_markdown_file(file_path: str, output_dir: str, gitbook_meta: bool):
+    print(f"Processing {file_path}")
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    # Regular expression pattern for matching the section
+    pattern = r'\`\`\`\w+ reference\n((?:https://)?github\.com/.*?)\n\`\`\`'
+    references = re.findall(pattern, content)
+
+    for reference_url in set(references):  # Use set to remove duplicates
+        values = reference_url.split('#L')
+        if len(values) != 2:
+            print(f"Invalid link: {reference_url} in file {file_path}")
+            continue
+
+        # Get the url and the lines
+        url, lines = values
+        start_end_line = lines.replace('L', '').split('-')
+        if len(start_end_line) != 2:
+            print(f"Invalid line format: {lines} in file {file_path}")
+            continue
+        start, end = start_end_line
+
+        # Parse the lines
+        start = int(start)
+        end = int(end)
+        if start < 0 or end < 0 or start >= end:
+            print(f"Invalid line range: {start}-{end} in file {file_path}")
+            continue
+        # Convert the reference url to a raw github url
+        raw_url = github_url_to_raw(url)
+
+        # Extract the section from the remote reference
+        res = requests.get(raw_url, allow_redirects=True)
+        reference_text = get_lines(res.text, start, end)
+
+        # Update the content with the reference
+        content = content.replace(
+            f" reference\n{reference_url}", f"\n{reference_text}")
+
+    # Save the file
+    parent = os.path.dirname(output_dir)
+    if not os.path.exists(parent):
+        os.makedirs(parent)
+
+    with open(output_dir, 'w') as file:
+        if gitbook_meta:
+            write_gitbook_meta(file)
+        file.write(content)
+    print(f"Saved to {output_dir}")
+
+
+def generate_docs(modules_dir: str, output_dir: str, gitbook_meta: bool = False):
+    # Check if the modules directory exists
+    if not os.path.isdir(modules_dir):
+        sys.exit('Error: The modules directory does not exist.')
+
+    # If output directory does not exist, create it
+    if not os.path.exists(output_dir) or not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+
+    # Recursively search for markdown files in the modules directory
+    for foldername, _, filenames in os.walk(modules_dir):
+        # Prevent analysis of .md files in sub directories
+        relative_folder = foldername.replace(modules_dir, "")
+        relative_path_split = relative_folder.split("/")
+        if len(relative_path_split) > 2:
+            continue
+
+        for filename in filenames:
+            if filename.endswith('.md'):
+                print(f"Processing {filename} in {foldername}")
+                file_path = os.path.join(foldername, filename)
+                output_path = file_path.replace(modules_dir, output_dir)
+                process_markdown_file(file_path, output_path, gitbook_meta)
+
