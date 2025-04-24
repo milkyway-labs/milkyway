f773aa0c25f182f0302cf9ff28cd24aecb81c6ab
Hanjun Kim <hallazzang@gmail.com>
2025-04-25 00:31:25 +0900

    chore: mint tokens

--- app/upgrades/v11/upgrades.go
+++ app/upgrades/v11/upgrades.go
@@ -4,11 +4,15 @@ import (
 	"context"
 
 	upgradetypes "cosmossdk.io/x/upgrade/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 
 	"github.com/milkyway-labs/milkyway/v10/app/keepers"
 	investorstypes "github.com/milkyway-labs/milkyway/v10/x/investors/types"
 )
+
+const foundationAddress = "milk108zdtldyt6r98rlg6la6nvwczzxnh2mjajlj4g"
 
 func CreateUpgradeHandler(
 	mm *module.Manager,
@@ -25,6 +29,21 @@ func CreateUpgradeHandler(
 		// UpdateInvestorsRewardRatio instead of SetInvestorsRewardRatio, just in case
 		// the investors were already delegating.
 		err = keepers.InvestorsKeeper.SetInvestorsRewardRatio(ctx, investorstypes.DefaultInvestorsRewardRatio)
+		if err != nil {
+			return nil, err
+		}
+
+		// Mint MILK token to the foundation account, so that we can distribute it later
+		foundationAddr, err := sdk.AccAddressFromBech32(foundationAddress)
+		if err != nil {
+			return nil, err
+		}
+		mintAmt := sdk.NewCoins(sdk.NewInt64Coin("umilk", 999_999_980)) // 1B - 20
+		err = keepers.BankKeeper.MintCoins(ctx, minttypes.ModuleName, mintAmt)
+		if err != nil {
+			return nil, err
+		}
+		err = keepers.BankKeeper.SendCoinsFromModuleToAccount(ctx, minttypes.ModuleName, foundationAddr, mintAmt)
 		if err != nil {
 			return nil, err
 		}
